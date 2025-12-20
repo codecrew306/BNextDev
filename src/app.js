6215fea5cb17ea950cc5fef2c735c39536d05970
Brito-Reji <britoreji03@gmail.com>
2025-12-20 13:38:52 +0530

    refactor: delete server startup and authentication routing files, and update package scripts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import authRoutes from './routes/auth.routes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,6 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', authRoutes);
+
 
 export default app;
